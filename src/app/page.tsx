f37e5bab9a845cb9116b36b2ccb55c24a4d69e3b
shandryll <shandryll94@gmail.com>
2025-04-08 20:45:51 -0300

    feat: create all landing page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+import { FAQ } from "./components/landing-page/FAQ";
 import { Header } from "./components/landing-page/Header";
 import { Hero } from "./components/landing-page/Hero";
+import { Pricing } from "./components/landing-page/Pricing";
+import { VideoExplanation } from "./components/landing-page/VideoExplanation";
 
 export default function Home() {
   return (
@@ -7,10 +10,9 @@ export default function Home() {
 
       <Header />
       <Hero />
-      {/* 
       <VideoExplanation />
       <Pricing />
-      <FAQ /> */}
+      <FAQ />
     </div>
   )
 }
